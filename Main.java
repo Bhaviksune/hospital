685826f9983c8a3f54d7d4d229527ca1ac019b27
Bhaviksune <118323028+Bhaviksune@users.noreply.github.com>
2025-01-10 11:12:33 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,1 @@
- class Main {
-    public static void main(String args[]){
-        System.out.println("m = 10") ;
-          System.out.println("n = 20") ;
-            System.out.println("m*n = 200") ;
-            
-      
 
-
-
-    }
-    
-}
