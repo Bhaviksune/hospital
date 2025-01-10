6a88b5b3990159238b5131e914009c2408fd035a
Bhaviksune <118323028+Bhaviksune@users.noreply.github.com>
2025-01-10 08:44:14 +0530

    Update Main .java

--- Main .java
+++ Main .java
@@ -1,10 +1,1 @@
- class Main {
-    public static void main(String args[]){
-        System.out.println("Enter length and breadth") ;
-          System.out.println("5 4") ;
-            System.out.println("Area = 20") ;
-             System.out.println("Perimeter = 18") ;
 
-    }
-    
-}
